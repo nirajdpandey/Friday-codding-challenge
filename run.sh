bb0fd96274bd5f9b4df62f2391fbdf228728030d
Niraj <nirajdevpandey@gmail.com>
2021-01-02 20:17:15 +0100

    updated bash

--- run.sh
+++ run.sh
@@ -2,11 +2,21 @@
 
 #########################################
 # Installing necessary packages and running the project
-# pyYaml
+# Additionally executing test cases
+# Python pyYaml will be installed using requirements file
 #########################################
 
 # You can install other packages using pip
 # However, there is already a requirements file which take cares of it
+
+printf "Installing requirements..... \n"
 pip install -r requirements.txt
-cd src
+cd src || { echo "could not switch to src folder"; exit 1; }
 python main.py
+printf "Running test cases...... \n"
+printf "Switching to test folder..... \n"
+cd .. || { echo "could not switch to test folder"; exit 1; }
+cd test || { echo "Please check your test folder path"; exit 1; }
+python test.py
+printf "Process finished! \n"
+
--- test/test.py
+++ test/test.py
@@ -13,7 +13,11 @@ import unittest
 import pathlib as pl
 
 # initialize logging
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+logging.basicConfig(filename='../logname.log',
+                    filemode='w',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 with open('../config.yml') as configuration:
